3d6c9f86f1ab4b173e3e0752d7540ebe690c4b58
Karthikeyan N <kart3128@global.chainsys.com>
2022-06-08 10:24:10 +0530

    add abstract and  default method

---
+++ src/com/chainsys/bestPractices/abstractDemo/ScoreBoard.java
@@ -0,0 +1,12 @@
+package com.chainsys.bestPractices.abstractDemo;
+
+public class ScoreBoard {
+	public int TargetScore = 250;
+	private ScoreBoard() {
+		System.out.println("Object created for ScoreBoard");
+	}
+	// createObject() is a factory method 
+	public static ScoreBoard createObject() {
+		return new ScoreBoard();
+	}
+}
